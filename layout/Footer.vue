17b839be551c8cd2cd86168954d6f29f75579e9b
jasdobilov <jasdobilov2@gmail.com>
2024-07-09 20:11:29 +0300

    Commit

--- layout/Footer.vue
+++ layout/Footer.vue
@@ -1,7 +1,7 @@
 <template>
 
 
-    <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+    <section class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
         <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
             <div class="sm:flex sm:items-center sm:justify-between">
                 <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
@@ -28,7 +28,7 @@
             <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a
                     href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
         </div>
-    </footer>
+    </section>
 
 
 
