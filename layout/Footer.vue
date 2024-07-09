a58305c92146a1b63ccd8edbf0073342378c1d54
jasdobilov <jasdobilov2@gmail.com>
2024-07-09 19:56:29 +0300

    dsdsd

--- layout/Footer.vue
+++ layout/Footer.vue
@@ -1,16 +1,26 @@
 <template>
-    <footer class="bg-gray-800 text-white py-6 px-3">
-        <div class="container mx-auto flex flex-wrap justify-between" style="align-items: center">
-            <div class="w-full md:w-1/2 md:text-center md:mb-0 mb-8">
-                <p class="text-xs text-gray-400 md:text-sm">Copyright 2024 &copy; All Rights Reserved</p>
-            </div>
-            <div class="w-full md:w-1/2 md:text-center md:mb-0 mb-8">
-                <ul class="list-reset flex justify-center flex-wrap text-xs md:text-sm gap-3">
-                    <li><a href="#" class="text-gray-400 hover:text-white">Contact</a></li>
-                    <li class="mx-4"><a href="#" class="text-gray-400 hover:text-white">Privacy Policy</a></li>
-                    <li><a href="#" class="text-gray-400 hover:text-white">Terms of Use</a></li>
-                </ul>
-            </div>
+
+
+    <footer class="bg-white rounded-lg shadow m-4 dark:bg-gray-800">
+        <div class="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/"
+                    class="hover:underline">Flowbite™</a>. All Rights Reserved.
+            </span>
+            <ul class="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">Contact</a>
+                </li>
+            </ul>
         </div>
     </footer>
+
 </template>
